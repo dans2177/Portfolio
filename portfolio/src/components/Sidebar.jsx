3d6e5c983238e7d9879bb5581e52e98b872a4597
Daniel <117490095+dans2177@users.noreply.github.com>
2023-12-14 09:03:21 -0700

    Update Sidebar.jsx

--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -6,19 +6,38 @@ function Sidebar() {
   return (
     <div className="fixed z-10 bottom-1 left-1 h-fit w-16  bg-orange-600  rounded-3xl text-white flex flex-col items-center justify-center m-4 p-6 shadow-lg">
       {/* Social media icons */}
-      <a href="https://www.github.com/dans2177" className="text-gray-200 text-2xl mb-4" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.github.com/dans2177"
+        className="text-gray-200 text-2xl mb-4"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaGithub />
       </a>
-      <a href="https://www.linkedin.com/in/danielshemon" className="text-gray-200 text-2xl mb-4" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.linkedin.com/in/daniel-shemon-0931b122a/"
+        className="text-gray-200 text-2xl mb-4"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaLinkedin />
       </a>
-      <a href="https://www.instagram.com/daniel_d_ridley" className="text-gray-200 text-2xl mb-4" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.instagram.com/daniel_d_ridley"
+        className="text-gray-200 text-2xl mb-4"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaInstagram />
       </a>
-      <a href="https://twitter.com/danielshemon" className="text-gray-200 text-2xl " target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://twitter.com/danieldridley"
+        className="text-gray-200 text-2xl "
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaTwitter />
       </a>
-
     </div>
   );
 }
