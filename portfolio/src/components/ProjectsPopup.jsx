c6ce18ccb94c62fb3c90f923bfc9f45ee3f89a68
Daniel <daniel@thebox3d.com>
2024-07-19 20:04:59 -0500

    Update ProjectsPopup.jsx

--- portfolio/src/components/ProjectsPopup.jsx
+++ portfolio/src/components/ProjectsPopup.jsx
@@ -15,76 +15,78 @@ function ProjectPopup({ project, closeModal }) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
-      <div className="bg-white dark:bg-gray-800 p-5 rounded-lg shadow-lg max-w-4xl w-full m-4">
-        <div className="flex justify-end">
-          <button
-            onClick={closeModal}
-            className="text-xl mb-4 p-1 hover:bg-gray-400 dark:bg-red-100 dark:hover:bg-red-500 rounded-full text-red-500 dark:hover:text-red-800"
-          >
-            <FaTimes />
-          </button>
-        </div>
-        <div className="grid grid-cols-1 gap-4">
-          <div className="slide-container">
-            <Slide {...properties}>
-              {project.images.map((each, index) => (
-                <div key={index} className="each-slide">
-                  <img
-                    src={each}
-                    alt={`Slide ${index}`}
-                    className="w-full h-auto rounded-2xl"
-                  />
-                </div>
-              ))}
-            </Slide>
+      <div className="max-w-4xl w-full m-4">
+        <div className="bg-white dark:bg-gray-800 p-5 rounded-lg shadow-lg max-h-screen overflow-y-auto">
+          <div className="flex justify-end">
+            <button
+              onClick={closeModal}
+              className="text-xl mb-4 p-1 hover:bg-gray-400 dark:bg-red-100 dark:hover:bg-red-500 rounded-full text-red-500 dark:hover:text-red-800"
+            >
+              <FaTimes />
+            </button>
           </div>
-          <div>
-            <h3 className="text-xl font-bold mb-2 font-comfortaa dark:text-white">
-              {project.title}
-            </h3>
-            <p className="mb-4 font-comfortaa dark:text-white">
-              {project.description}
-            </p>
-            {project.features && (
-              <>
-                <h4 className="text-lg font-semibold mb-2 text-gray-900 dark:text-white">
-                  Key Features:
-                </h4>
-                <ul className="list-none mb-4 text-gray-800 dark:text-gray-300 space-y-4">
-                  {project.features.map((feature, index) => (
-                    <li key={index} className="flex items-start space-x-2">
-                      <div>
-                        <strong className="text-blue-600 dark:text-blue-400">
-                          {feature.name}:
-                        </strong>
-                        <span className="ml-1">{feature.details}</span>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            )}
-            <div className="flex flex-col sm:flex-row gap-3 pt-4">
-              {project.demo && (
-                <a
-                  href={project.demo}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center justify-center bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
-                >
-                  <FaPlay className="mr-2" size={22} /> Demo
-                </a>
+          <div className="grid grid-cols-1 gap-4 p-4">
+            <div className="slide-container">
+              <Slide {...properties}>
+                {project.images.map((each, index) => (
+                  <div key={index} className="each-slide">
+                    <img
+                      src={each}
+                      alt={`Slide ${index}`}
+                      className="w-full h-auto rounded-2xl"
+                    />
+                  </div>
+                ))}
+              </Slide>
+            </div>
+            <div>
+              <h3 className="text-xl font-bold mb-2 font-comfortaa dark:text-white">
+                {project.title}
+              </h3>
+              <p className="mb-4 font-comfortaa dark:text-white">
+                {project.description}
+              </p>
+              {project.features && (
+                <>
+                  <h4 className="text-lg font-semibold mb-2 text-gray-900 dark:text-white">
+                    Key Features:
+                  </h4>
+                  <ul className="list-none mb-4 text-gray-800 dark:text-gray-300 space-y-4">
+                    {project.features.map((feature, index) => (
+                      <li key={index} className="flex items-start space-x-2">
+                        <div>
+                          <strong className="text-blue-600 dark:text-blue-400">
+                            {feature.name}:
+                          </strong>
+                          <span className="ml-1">{feature.details}</span>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </>
               )}
-              {project.link && (
-                <a
-                  href={project.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center justify-center bg-orange-600 text-white py-2 px-4 rounded hover:bg-orange-900 transition"
-                >
-                  <FaGithub className="mr-2" size={28} /> GitHub
-                </a>
-              )}
+              <div className="flex flex-col sm:flex-row gap-3 pt-4">
+                {project.demo && (
+                  <a
+                    href={project.demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center justify-center bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
+                  >
+                    <FaPlay className="mr-2" size={22} /> Demo
+                  </a>
+                )}
+                {project.link && (
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center justify-center bg-orange-600 text-white py-2 px-4 rounded hover:bg-orange-900 transition"
+                  >
+                    <FaGithub className="mr-2" size={28} /> GitHub
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         </div>
